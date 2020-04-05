9ddf5b44df3da8a83e9b8d6e5a50e3400e59957d
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-04 23:45:30 -0400

    changes how the page is displayed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
-import DesktopContainer from './components/Home'
+import Home from './components/Home'
 import LinesContainer from './components/LinesContainer'
 import { Switch, Route } from 'react-router-dom';
 import { withRouter } from 'react-router-dom'
@@ -27,18 +27,51 @@ class App extends React.Component {
 
 
 
-
-    fetch("http://localhost:4000/lines")
+    // will live inside the stop component in a component did mount function
+    fetch("http://localhost:4000/lines/13")
     .then(r => r.json())
-    .then((lines) => {
-      this.props.setAllLines(lines)
+    .then((line) => {
+      // diggs through feed to find the arrays with the arrival times
+      let feed = line.feed.filter( obj => Object.keys(obj).includes("trip_update"))
+      let includesStopTimeUpdate = feed.filter(obj => Object.keys(obj.trip_update).includes("stop_time_update"))
+      let stopTimeUpdateArrays = includesStopTimeUpdate.map(obj => obj.trip_update.stop_time_update)
+      let stopTimeArrays = stopTimeUpdateArrays.map(obj => obj.map(obj2 => obj2))
+
+      let trainObjs = []
+
+      // adds the objects with train arrival times and stop ids to "state"
+      stopTimeArrays.map(obj => {
+        obj.map(obj2 => {
+          trainObjs.push(obj2)
+        })
+      })
+
+      let arrivalTimes = trainObjs.filter(obj => obj.stop_id.includes("726N"))
+      let trainArrivalsInMinutes = arrivalTimes.map(obj => {
+        let myDate = new Date( parseInt(obj.arrival.time) *1000);
+        let today = new Date
+
+
+        // checks for trains coming in the next hour
+        // if the train arrival minus the time now is negative add 60 mins
+        // return myDate.getMinutes() - today.getMinutes()
+        if (Math.sign(myDate.getMinutes() - today.getMinutes()) === -1) {
+          return (myDate.getMinutes() - today.getMinutes()) + 60
+        } else {
+          return myDate.getMinutes() - today.getMinutes()
+        }
+      })
+
+      console.log(trainArrivalsInMinutes);
     })
 
 
    }
 
 
+  stopTimeLookUp = (line) => {
 
+  }
 
 
   handleLoginSubmit = (userInfo) => {
@@ -112,7 +145,7 @@ class App extends React.Component {
           <Route path="/login" render={ this.renderForm } />
           <Route path="/signin" render={ this.renderForm } />
           <Route path="/profile" render={ this.renderProfile } />
-          <Route path="/" exact render={() => <LinesContainer /> } />
+          <Route path="/" exact render={() => <Home /> } />
           <Route render={ () => <p>Page not Found</p> } />
         </Switch>
       </div>
