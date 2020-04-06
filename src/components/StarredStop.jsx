95caaef331377927fb326f06109360f59f4b11aa
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-06 19:08:43 -0400

    adds StarredStop component

---
+++ src/components/StarredStop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StarredStop = (props) => {
+
+
+  return(
+    <h1>one starredstop</h1>
+  )
+};
+
+export default StarredStop;
--- src/components/StarredStopsContainer.jsx
+++ src/components/StarredStopsContainer.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import StopCard from './StopCard'
+import StarredStop from './StarredStop';
 import { List, Icon, Button } from 'semantic-ui-react';
 
 class StarredStopsContainer extends Component {
 
   renderStops = () => {
     return this.props.user.user_stops.map(stopObj => {
-      return <StopCard
+      return <StarredStop
                 key={ stopObj.id }
                 stop={ stopObj }
               />
