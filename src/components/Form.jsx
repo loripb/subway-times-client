526f17d01096767b3079a517f756417c4a186087
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-07 16:35:44 -0400

    adds link for login and sign up page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,7 +20,6 @@ class FormContainer extends Component {
   }
 
   render() {
-    let { username, password } = this.state
 
     return (
         <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
@@ -49,9 +48,9 @@ class FormContainer extends Component {
               {
                 this.props.formName === "Login"
                 ?
-                <div>New to us? <a href='#'>Sign Up</a></div>
+                <div>New to us? <a href='http://localhost:3000/signup'>Sign Up</a></div>
                 :
-                <div>Already have an account? <a href='#'>Login</a></div>
+                <div>Already have an account? <a href='http://localhost:3000/login'>Login</a></div>
               }
             </Message>
           </Grid.Column>
