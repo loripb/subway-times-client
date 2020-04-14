5ea8107c1a7040e221d89d0f67edd31a0b7043af
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-14 17:27:39 -0400

    changes StarredStopsContainer into a functional component

--- src/components/StarredStopsContainer.jsx
+++ src/components/StarredStopsContainer.jsx
@@ -5,43 +5,39 @@ import { withRouter } from 'react-router-dom';
 import StarredStop from './StarredStop';
 import { List, Button, Icon, Image } from 'semantic-ui-react';
 
-class StarredStopsContainer extends Component {
+const StarredStopsContainer = (props) => {
 
-  renderStops = () => {
-    return this.props.user.user_stops.map(stopObj => {
+  const renderStops = () => {
+    return props.user.user_stops.map(stopObj => {
       return <StarredStop
                 key={ stopObj.id }
                 stop={ stopObj }
-                user={ this.props.user }
+                user={ props.user }
               />
     })
   }
 
-  handleClick = () => {
-    this.props.changeDirection()
+  const handleClick = () => {
+    props.changeDirection()
   }
 
-
-  render(){
-    return (
-      <>
-        <h3>Starred Stops</h3>
-        <Icon className='exchange' onClick={ this.handleClick } color="orange" size='large'/>
-        {
-          this.props.user.starred_stops.length > 0
-          ?
-          <List celled relaxed='very'>
-            {
-              this.renderStops()
-            }
-          </List>
-          :
-          <h5>You have no starred stops. Add a stop by clicking the star net to a stop.</h5>
-        }
-      </>
-    );
-  }
-
+  return (
+    <>
+      <h3>Starred Stops</h3>
+      <Icon className='exchange' onClick={ handleClick } color="orange" size='large'/>
+      {
+        props.user.starred_stops.length > 0
+        ?
+        <List celled relaxed='very'>
+          {
+            renderStops()
+          }
+        </List>
+        :
+        <h5>You have no starred stops. Add a stop by clicking the star net to a stop.</h5>
+      }
+    </>
+  );
 }
 
 const mapStateToProps = (reduxState) => {
