53c8363e62fbcaf58c0a1bfcfe34c4aeefef5aa2
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-14 17:08:36 -0400

    changes StopContainer component to a functional component

--- src/components/StopsContainer.jsx
+++ src/components/StopsContainer.jsx
@@ -1,46 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { changeDirection } from '../Redux/actions'
 import StopCard from './StopCard'
 import { List, Icon } from 'semantic-ui-react'
 
+const StopsContainer = ({ line, handleRenderChange }, props) => {
 
-
-class StopsContainer extends Component {
-
-  state = {
-    direction: "N"
+  const handleDirectionChange = () => {
+    props.changeDirection()
   }
 
-  handleDirectionChange = () => {
-    this.props.changeDirection()
-  }
-
-  renderStops = () => {
-    return this.props.line.stops.map(stopObj => {
+  const renderStops = () => {
+    return line.stops.map(stopObj => {
       return <StopCard
-                key={ stopObj.id }
-                line={ this.props.line }
-                stop={ stopObj }
-                triggerRender={ this.props.triggerRender }
-              />
+        key={ stopObj.id }
+        line={ line }
+        stop={ stopObj }
+        triggerRender={ props.triggerRender }
+      />
     })
   }
 
-
-  render() {
-    return (
-      <div className="stop_container">
-        <h2>{ this.props.line.name } Train Stops { this.props.direction === "N" ? "Uptown" : "Downtown" }</h2>
-        <Icon className='exchange' onClick={ this.handleDirectionChange } color="orange" size='large'/>
-        <List celled relaxed='very'>
-          { this.renderStops() }
-        </List>
-      </div>
-    );
-  }
-
+  return (
+    <div className="stop_container">
+      <h2>{ line.name } Train Stops { props.direction === "N" ? "Uptown" : "Downtown" }</h2>
+      <Icon className='exchange' onClick={ handleDirectionChange } color="orange" size='large'/>
+      <List celled relaxed='very'>
+        { renderStops() }
+      </List>
+    </div>
+  );
 }
 
 const mapStateToProps = (reduxState) => {
