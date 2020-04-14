ea8e74e255f11a1c3651d4617719d734428ea6a8
Lori Boyd <lori.p.boyd@gmail.com>
2020-04-14 17:18:11 -0400

    changes LinesContainer component to a functional component

--- src/components/LinesContainer.jsx
+++ src/components/LinesContainer.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LineCard from './LineCard'
 import { List, Grid } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class LinesContainer extends Component {
+const LinesContainer = (props) => {
 
-  renderLines = () => {
+  const renderLines = () => {
     // set colums
-    const colum1 = this.props.lines.slice(0, 3)
-    const colum2 = this.props.lines.slice(3, 6)
-    const colum3 = this.props.lines.slice(6, 9)
-    const colum4 = this.props.lines.slice(9, 12)
-    const colum5 = this.props.lines.slice(12, 15)
-    const colum6 = this.props.lines.slice(15, 18)
-    const colum7 = this.props.lines.slice(18, 21)
-    const colum8 = this.props.lines.slice(21)
+    const colum1 = props.lines.slice(0, 3)
+    const colum2 = props.lines.slice(3, 6)
+    const colum3 = props.lines.slice(6, 9)
+    const colum4 = props.lines.slice(9, 12)
+    const colum5 = props.lines.slice(12, 15)
+    const colum6 = props.lines.slice(15, 18)
+    const colum7 = props.lines.slice(18, 21)
+    const colum8 = props.lines.slice(21)
 
     // set rows
     const row1 = () => {
@@ -23,21 +23,21 @@ class LinesContainer extends Component {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row2 = () => {
       return colum2.map(lineObj => {return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }/>})
+        handleRenderChange={ props.handleRenderChange }/>})
     }
     const row3 = () => {
       return colum3.map(lineObj => {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row4 = () => {
@@ -45,7 +45,7 @@ class LinesContainer extends Component {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row5 = () => {
@@ -53,7 +53,7 @@ class LinesContainer extends Component {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row6 = () => {
@@ -61,7 +61,7 @@ class LinesContainer extends Component {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row7 = () => {
@@ -69,14 +69,14 @@ class LinesContainer extends Component {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
     const row8 = () => { return colum8.map(lineObj => {
       return <LineCard
         line={ lineObj }
         key={ lineObj.id}
-        handleRenderChange={ this.props.handleRenderChange }
+        handleRenderChange={ props.handleRenderChange }
       />
   })}
 
@@ -108,14 +108,11 @@ class LinesContainer extends Component {
           </Grid>
   }
 
-  render() {
-    return (
-      <div>
-        {this.renderLines()}
-      </div>
-    );
-  }
-
+  return (
+    <div>
+      {renderLines()}
+    </div>
+  );
 }
 
 const mapStateToProps = (reduxState) => {
