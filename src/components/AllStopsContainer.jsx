3877c5f99e7ea833492749b887a87bc463ad8ebe
Lori Boyd <lori.p.boyd@gmail.com>
2020-05-02 19:17:49 -0400

    adds stop info to Single stop

--- src/components/AllStopsContainer.jsx
+++ src/components/AllStopsContainer.jsx
@@ -17,14 +17,22 @@ const AllStopsContainer = (props) => {
     // iterate through allStops
     // if the stop_id does not include a N or S add to new array
     const newStops = props.allStops.filter(stop => (stop.stop_id.includes('N') === false) && (stop.stop_id.includes('S') === false))
-    return newStops.map(stop => stop.name)
+    return newStops.map(stop => stop)
   }
 
   const filterWithSearchParams = (stopsArr) => {
     // filter through new array with searchParams
-    const stopsWithSearch = searchParams ? stopsArr.filter(stop => stop.toLowerCase().includes(searchParams.toLowerCase())) : []
+    const stopsWithSearch = searchParams ? stopsArr.filter(stop => stop.name.toLowerCase().includes(searchParams.toLowerCase())) : []
+
+    // make an array of only names
+    let stopNames = stopsWithSearch.map(stop => stop.name)
     // remove duplicates
-    return Array.from(new Set(stopsWithSearch))
+    stopNames = Array.from(new Set(stopNames))
+
+    // map over that and compare to objs in stopsWithSearch
+    // if the name matches with the obj make a new obj with the id as an array + add to it
+    let stopArr = (stopNames.map(stopName => stopsWithSearch.filter(stop2 => stop2.name == stopName)))
+    return stopArr
   }
 
   const renderStops = () => {
@@ -33,8 +41,9 @@ const AllStopsContainer = (props) => {
     return filteredStops.map(stop => {
       return(
         <SingleStop
-          key={ stop }
-          stopName={ stop }
+          key={ stop[0].id }
+          stopName={ stop[0].name }
+          stopId={ stop[0].stop_id }
         />
       )
     })
